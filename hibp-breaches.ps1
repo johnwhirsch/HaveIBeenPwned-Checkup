b6c88dbfb55da884f65a92dad19c857420ed4469
DRENREPUS <johnwhirsch@gmail.com>
2020-02-21 12:28:40 -0500

    Fixed the pastes and added examples

--- hibp-breaches.ps1
+++ hibp-breaches.ps1
@@ -1,31 +1,34 @@
+# To use, run this powershell file with -pwnedrecords argument
+# Examples:
+# .\hibp-breaches.ps1 -pwnedrecords "https://haveibeenpwned.com/DomainSearch/bbe6cc98ea31222222222d7ab9f0997/Json"
+# .\hibp-breaches.ps1 -pwnedrecords "\\contoso.com\shares\report.json"
+# .\hibp-breaches.ps1 -pwnedrecords "C:\Users\Contoso\Desktop\report.json"
+#
+
 param([Parameter(Mandatory=$True)][string]$pwnedrecords)
 
-# Get the JSON file no matter where it is
 if($pwnedrecords -like "http*"){
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     Write-Host "Attempting to get records from web JSON: $($pwnedrecords)";
     $breachesjson = [System.Net.WebClient]::new().DownloadString("$pwnedrecords") | ConvertFrom-Json
 }
-elseif($pwnedrecords -like "\\*"){ $breachesjson = Get-Content "Microsoft.PowerShell.Core\FileSystem::$($pwnedrecords)" }
-else{ $breachesjson = Get-Content $pwnedrecords }
-
-
-# This will set how many days back the script should start searching for old passwords
-[int]$daysback = 30;
+elseif($pwnedrecords -like "\\*"){ $breachesjson = Get-Content "Microsoft.PowerShell.Core\FileSystem::$($pwnedrecords)" | ConvertFrom-Json }
+else{ $breachesjson = Get-Content $pwnedrecords | ConvertFrom-Json }
 
 $allAdUsers = $(get-aduser  -LDAPFilter "(&(objectCategory=person)(objectClass=user)(!userAccountControl:1.2.840.113556.1.4.803:=2))" -properties * )
 
 $badbreachcount=0;
+$badpastecount=0;
 
 foreach($breach in $breachesjson.BreachSearchResults){
 
-    $badbreachs = $breach.Breaches | ? { ($_.Description -like "*credential*" -or $_.Description -like "*password*") -and $_.Description -notlike "*no password*" }
-    if($badbreachs.count -gt 0 -or $($breach.Pastes).count -gt 0){
+    $badbreaches = $breach.Breaches | ? { ($_.Description -like "*credential*" -or $_.Description -like "*password*") -and $_.Description -notlike "*no password*" }
+    if($badbreaches.count -gt 0){
 
         if(( $adusercheck = $allAdUsers | ? { $_.SamAccountName -eq $($breach.Alias) } )){         
             
 
-            foreach($badbreach in $badbreachs){                   
+            foreach($badbreach in $badbreaches){                   
                 try{
                     if($adusercheck.PasswordLastSet -lt [datetime]$badbreach.BreachDate -and $adusercheck.Enabled -eq $true){
                         $badbreachcount = $badbreachcount + 1;
@@ -51,24 +54,50 @@ foreach($breach in $breachesjson.BreachSearchResults){
                     }
                 }catch{ Write-Output "Unable to find breach alias against AD."; continue }
             }
-
-            #work in progress, not functional yet
-            if($($badbreach.Pastes).count -gt 0){
-                           
-                foreach($paste in $badbreach.Pastes){
-                    Write-Host "[" -ForegroundColor Red -NoNewline;
-                    Write-Host "$($paste.Source) | $($paste.Date)" -ForegroundColor Yellow -NoNewline;
-                    Write-Host "]" -ForegroundColor Red -NoNewline;
-                    Write-Host "User credentials were found pasted on $($paste.Source)"
-
-                }
-                Write-Host "";
-            }
+           
         }
     
     }
 
 }
+foreach($breach in $breachesjson.PasteSearchResults){
+    if($($breach.Pastes).count -gt 0){
+        if(( $adusercheck = $allAdUsers | ? { $_.SamAccountName -eq $($breach.Alias) } )){
+         
+            foreach($paste in $breach.Pastes){
+
+                if($($paste.Date).length -eq 0){ $pasteDate = "2222-01-01T00:13:37Z"; }
+                else{ $pasteDate = $paste.Date }
+                if($adusercheck.PasswordLastSet -lt [datetime]$pasteDate -and $adusercheck.Enabled -eq $true){
+                    $badpastecount=$badpastecount+1;
+                    if($pasteDate -eq "2222-01-01T00:13:37Z"){ $pasteDate = "Unknown Post Date" }
+
+                        $csvout = new-object PSObject
+                        $csvout | add-member -membertype NoteProperty -name "UserAccount" -value $($adusercheck.SamAccountName)
+                        $csvout | add-member -membertype NoteProperty -name "UserDescription" -value $($adusercheck.DisplayName)
+                        $csvout | add-member -membertype NoteProperty -name "LastPwChange" -value $($adusercheck.PasswordLastSet)
+                        $csvout | add-member -membertype NoteProperty -name "BreachTitle" -value "PASTED - $($paste.Source)"
+                        $csvout | add-member -membertype NoteProperty -name "BreachDate" -value $($pasteDate)
+                        $csvout | add-member -membertype NoteProperty -name "BreachDescription" -value "User credentials were found pasted on $($paste.Source)"
+                        $csvout | Export-Csv "$($PSScriptRoot)\BreachesReport.csv" -Append -notypeinformation
+
+                        write-host "Found breach for user [ " -ForegroundColor Red -NoNewline;
+                        write-host "$($adusercheck.SamAccountName) | $($adusercheck.DisplayName)" -ForegroundColor Yellow -NoNewline;
+                        write-host " ] " -ForegroundColor Red -NoNewline;
+                        write-host "Last PW change: " -ForegroundColor Red -NoNewline;
+                        write-host "$($adusercheck.PasswordLastSet)" -foregroundcolor yellow;
 
 
-Write-Host "Total bad breachs in the last $($daysback) days: $($badbreachcount)" -ForegroundColor Yellow -BackgroundColor DarkRed
+                        Write-Host "[ " -ForegroundColor Red -NoNewline;
+                        Write-Host "$($paste.Source) | $($pasteDate)" -ForegroundColor Yellow -NoNewline;
+                        Write-Host " ] " -ForegroundColor Red -NoNewline;
+                        Write-Host "User credentials were found pasted on $($paste.Source)"
+                }
+
+            }
+        }
+    }
+}
+
+Write-Host "Total bad breaches found: $($badbreachcount)" -ForegroundColor Yellow -BackgroundColor DarkRed
+Write-Host "Total bad pastes found: $($badpastecount)" -ForegroundColor Yellow -BackgroundColor DarkRed
